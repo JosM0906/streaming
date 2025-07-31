6fac182c528dc3f1d1c3a744523835c0005126a9
JosM0906 <eosorioi1@miumg.edu.gt>
2025-07-30 18:47:51 -0600

    cambio en quimica.jsx

---
+++ src/pages/Categorias/Quimica.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import HeroBanner from '../../components/HeroBanner/HeroBanner';
+import TitleCards from '../../components/TitleCards/TitleCards';
+import bannerImg from '../../assets/quimica-banner.jpg'; // tu imagen destacada
+
+const Quimica = () => {
+  return (
+    <div className="categoria-page">
+      <HeroBanner
+        background={bannerImg}
+        title="Química en tu mundo"
+        description="Descubre cómo la química está presente en tu día a día."
+      />
+      <TitleCards title="Videos destacados de Química" category="quimica" />
+    </div>
+  );
+};
+
+export default Quimica;
