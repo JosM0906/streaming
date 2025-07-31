c9a662b0a757730f367f2dd575079416d99ebac7
JosM0906 <eosorioi1@miumg.edu.gt>
2025-07-30 19:51:56 -0600

    actualizacion en HeroBanner

--- src/components/HeroBanner/HeroBannerQuimica.jsx
+++ src/components/HeroBanner/HeroBannerQuimica.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './HeroBanner.css';
+import bannerImg from '../../assets/bannerQuimica.jpg'
 
 const HeroBannerQuimica = () => {
   return (
-    <div className="hero-banner">
+    <div
+      className="hero-banner"
+      style={{ backgroundImage: `url(${bannerImg})` }}
+    >
       <div className="overlay">
         <h1>Química en la vida diaria</h1>
         <p>Descubre cómo reacciones químicas ocurren en tu entorno.</p>
